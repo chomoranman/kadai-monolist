9f44f0ddcd4d17ed0a4b9dc43081b726baf67681
chomo <program4200@gmail.com>
2018-09-12 09:39:35 +0000

    rakuten api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     password = params[:session][:password]
     if login(email, password)
       flash[:success] = 'ログインに成功しました。'
-      redirect_to @users
+      redirect_to @user
     else
       flash.now[:danger] = 'ログインに失敗しました。'
       render :new
@@ -25,7 +25,7 @@ class SessionsController < ApplicationController
   def login(email, password)
     @user = User.find_by(email: email)
     if @user && @user.authenticate(password)
-      session[user_id] = @user.id
+      session[:user_id] = @user.id
       return true
     else
       return false
