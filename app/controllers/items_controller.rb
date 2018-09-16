97615a769d4956c9af6563becc4ace46421c6125
chomo <program4200@gmail.com>
2018-09-16 10:52:28 +0000

    kadai ranking

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,16 +4,25 @@ class ItemsController < ApplicationController
     
     @keyword = params[:keyword]
     if @keyword.present? # 
-      results = RakutenWebService::Ichiba::Item.search({
-        keyword: @keyword,
-        imageFlag: 1,
-        hits: 20,
-      })
+      keyword_ary = @keyword.gsub('　', ' ').split(' ')
+      @keyword = keyword_ary.each{ |o| keyword_ary.delete(o) if o.size <= 1}.join(' ')
+      render 'new' if @keyword.nil?
       
-      results.each do |result|
-        item = Item.find_or_initialize_by(read(result))
-        @items << item
+      if @keyword.present?
+        results = RakutenWebService::Ichiba::Item.search({
+          keyword: @keyword,
+          imageFlag: 1,
+          hits: 20,
+        })
+        
+        results.each do |result|
+          item = Item.find_or_initialize_by(read(result))
+          @items << item
+        end
       end
+    end
+    if @items.blank?
+      flash.now[:danger] = "見つかりません。"
     end
   end
   
